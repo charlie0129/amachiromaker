bcb290e830703ef7995b92070fa9459621363199
Charlie Chiang <charlie_c_0129@outlook.com>
2021-11-22 19:46:25 +0800

    feat: display default combination

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LayerImage } from './types';
+import services from './services';
+import consts from './consts';
+
 
 function App() {
+  const [layerComb, setLayerComb] = useState<LayerImage[]>();
+
+  useEffect(() => {
+    services.getDefaultCombination()
+      .then(res => {
+        setLayerComb(res)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="image-area" style={{ border: "dashed red" }}>
+        {
+          layerComb?.map((layer, idx) => (
+            <img
+              src={`${consts.CDN_PREFIX}${layer.url}`}
+              className="layer-image"
+              style={{
+                left: layer.x,
+                top: layer.y,
+                zIndex: 500 + idx
+              }}
+            />
+          ))
+        }
+      </div>
+      <div className="config-area" style={{ border: "dashed blue" }}>
+
+      </div>
     </div>
   );
 }
